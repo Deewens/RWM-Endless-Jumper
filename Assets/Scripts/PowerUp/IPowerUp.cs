18829f039994b92995b13feafb4efff8685e112c
Adrien Dudon <c00278154@itcarlow.ie>
2022-10-10 12:05:27 +0100

    Add basic power-up interface

---
+++ Assets/Scripts/PowerUp/IPowerUp.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace PowerUp
+{
+    public interface IPowerUp
+    {
+        PowerUpType Type { get; }
+
+        /**
+         * Instantiate a new PowerUp in the level
+         */
+        void Spawn();
+    }
+}
