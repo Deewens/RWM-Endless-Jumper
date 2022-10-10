84c0dcb0bc5e3eace1419c5ee5a2e237ae235612
JackSinnott <c00242525@itcarlow.ie>
2022-10-10 11:05:45 +0100

    Add coin animation RWMPA22-21
    Coin spawning and score updating
    
    Removed debug comments from console
    
    Small bug fixes

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public float speed;
+    public bool alive = true;
+    private int coinValue; // the added score value for a coin
+    private Rigidbody2D rb;
+
+    private GameManager scoreManager;
+
+    private void Awake()
+    {
+        scoreManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+        rb = GetComponent<Rigidbody2D>();
+        coinValue = 10;
+    }
+    private void Update()
+    {
+        Move();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            alive = false;
+            scoreManager.setScore(coinValue);
+            Destroy(this.gameObject);
+            Debug.Log("Player hit \n");
+        }
+    }
+
+    private void Move()
+    {
+        Vector2 velocity = new Vector2(speed, 0) * Time.deltaTime;
+        rb.velocity -= velocity;
+    }
+
+
+
+}
