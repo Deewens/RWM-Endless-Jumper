038d89d8d82f0eb24b41353d6ca4e2f8aa838cc5
Adrien Dudon <c00278154@itcarlow.ie>
2022-10-17 13:35:36 +0100

    Read data and add them to external google form

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject level;
+
+    private DataManager _dataManager;
     
     public TMPro.TextMeshProUGUI  scoreText;
     public static int score = 0;
     private GameObject[] coins;
     private bool scoreDoubled = false;
 
-
+    private float _startTime;
+    private float _dieTime;
+    
     private void Start()
     {
+        _startTime = Time.time;
+        
+        _dataManager = GameObject.FindGameObjectWithTag("DataManager").GetComponent<DataManager>();
+        
         coins = GameObject.FindGameObjectsWithTag("Coin");
         scoreText.text = "Score: " + score;
     }
@@ -37,13 +46,22 @@ public class GameManager : MonoBehaviour
     
     public void ResetGame()
     {
-        LevelController levelController = level.GetComponent<LevelController>();
+        SceneManager.LoadScene("MainMenu");
+        _dieTime = Time.time;
+
+        float storeTime = _dieTime - _startTime;
+        _dataManager.AddDeath();
+        _dataManager.UpdateHighestScore(score);
+        _dataManager.UpdateLongestTimePlayed(storeTime);
+
+        score = 0;
+        
+        /*LevelController levelController = level.GetComponent<LevelController>();
         level.transform.position = levelController.StartPosition;
         foreach (GameObject c in coins)
         {
             c.GetComponent<Coin>().ResetCoins();
-        }
-        score = 0;
+        }*/
     }
     
     public void DoubleScore()
