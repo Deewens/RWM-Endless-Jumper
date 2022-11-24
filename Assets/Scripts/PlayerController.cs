95bd3dd0ac4daba43b88193b8517dad50cd23e63
Adrien <dudonadrien@gmail.com>
2022-11-24 21:19:16 +0000

    Add animations (idle, running, jumping) to player RWMPA22-34
    Add new player to the scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,35 +4,69 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+public enum PlayerAnimStates
+{
+    Idle = 0,
+    Running = 1,
+    Jumping = 2,
+}
+
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float jumpForce;
-    [SerializeField] private LayerMask _groundMask;
+    [SerializeField] private LayerMask groundMask;
     
+    public PlayerAnimStates playerAnimState;
+    private static readonly int State = Animator.StringToHash("State");
+
+    private Animator _animator;
     private Rigidbody2D _rb;
+    private CapsuleCollider2D _capsuleCollider;
     
     private bool _verticalInput; // Jump
     private bool _isJumping;
-    
+
     private GameManager _gameManager;
+
+    private Vector2 _rayPosition;
+    [SerializeField] private float rayDistance = 0.05f;
 
     void Start()
     {
         _gameManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
         _rb = GetComponent<Rigidbody2D>();
+        _capsuleCollider = GetComponent<CapsuleCollider2D>();
+        _animator = GetComponent<Animator>();
+        
+        _rayPosition = transform.position;
     }
 
     void Update()
     {
         GameOver();
         Jump();
+
+        if (IsGrounded())
+        {
+            playerAnimState = PlayerAnimStates.Running;
+        }
+        else
+        {
+            playerAnimState = PlayerAnimStates.Jumping;
+        }
+        
+        _rayPosition = transform.position;
+        /*_rayPosition.y -= 0.5f;
+        _rayPosition.x -= 0.5f;*/
+        Debug.DrawRay(_capsuleCollider.bounds.center, Vector2.down * rayDistance, Color.yellow);
+        _animator.SetInteger(State, (int)playerAnimState);
     }
 
     private void Jump()
     {
         if (!_verticalInput || !IsGrounded()) return;
 
-        _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        _rb.velocity = Vector2.up * jumpForce;
     }
 
     private void OnMove(InputValue value)
@@ -45,18 +79,11 @@ public class PlayerController : MonoBehaviour
 
     private bool IsGrounded()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.8f, _groundMask);
-        return hit.collider != null;
-    }
+        RaycastHit2D raycastHit = Physics2D.BoxCast(_capsuleCollider.bounds.center, _capsuleCollider.bounds.size, 0,
+            Vector2.down, rayDistance, groundMask);
 
-    private void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.collider.CompareTag("Saw"))
-        {
-            _gameManager.ResetGame();
-        }
+        return raycastHit.collider != null;
     }
-
     private void GameOver()
     {
         if(transform.position.y < -4.5)
