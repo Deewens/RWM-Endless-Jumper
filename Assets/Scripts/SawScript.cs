769414370fb27576affb8669f114d3a9e8bc8c44
Izabela-Zelek <c00247865@itcarlow.ie>
2022-11-28 13:08:20 +0000

    Coin no longer overlaps with saw - RWMPA22-70

--- Assets/Scripts/SawScript.cs
+++ Assets/Scripts/SawScript.cs
@@ -22,7 +22,7 @@ public class SawScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.CompareTag("Player"))
+        if (col.CompareTag("Player") || col.CompareTag("Coin"))
         {
             Destroy(col.gameObject);
         }
