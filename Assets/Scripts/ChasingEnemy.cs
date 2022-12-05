0f6bdbc096afee33a95fa3bcfaab66e38d147fdd
JackSinnott <45257691+JackSinnott@users.noreply.github.com>
2022-12-05 12:11:24 +0000

    Enemy now moves towards player (no teleporting)  RWMPA22-83
    Added a variable value to allow us to dynamically control the rate of chasing

--- Assets/Scripts/ChasingEnemy.cs
+++ Assets/Scripts/ChasingEnemy.cs
@@ -7,14 +7,20 @@ public class ChasingEnemy : MonoBehaviour
     [field: SerializeField]
     public float OffsetFromPlayerStable { get; private set; } = 6f;
     
+    [field: SerializeField]
     public float OffsetFromPlayerUnstable { get; private set; } = 2f;
 
+    [SerializeField]
     private PlayerDamageSystem _pds;
 
     [SerializeField] private PlayerController _player;
 
+    [SerializeField] private bool m_angered = false;
+
     private Vector2 _stablePosition;
     private Vector2 _unstablePosition;
+
+    [SerializeField] private int _duration = 5;
 
 
     private void Start()
@@ -33,18 +39,24 @@ public class ChasingEnemy : MonoBehaviour
         if (_pds._playerDamage == 1)
         {
             _pds.SetDamage(0);
-            transform.position = _unstablePosition;
-            StartCoroutine(LerpPosition(_stablePosition, 5));
+            if(!m_angered)
+                StartCoroutine(LerpPosition(_unstablePosition, _duration));
         }
     }
 
     IEnumerator LerpPosition(Vector2 targetPosition, float duration)
     {
         float time = 0;
-        Vector2 startPosition = transform.position;
         while (time < duration)
         {
-            transform.position = Vector2.Lerp(startPosition, targetPosition, time / duration);
+            transform.position = Vector2.Lerp(_stablePosition, targetPosition, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        time = 0;
+        while (time < duration)
+        {
+            transform.position = Vector2.Lerp(targetPosition, _stablePosition, time / duration);
             time += Time.deltaTime;
             yield return null;
         }
