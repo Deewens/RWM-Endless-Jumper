9af8e299bbe7a8b26c1a99c74872db597ee34fe6
Adrien Dudon <dudonadrien@gmail.com>
2022-12-12 11:27:45 +0000

    Missile spawn more frequently as time passes, increasing difficulty RWMPA22-97

--- Assets/Scripts/Missiles/MissilesManager.cs
+++ Assets/Scripts/Missiles/MissilesManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -6,33 +8,56 @@ namespace Missiles
 {
     public class MissilesManager : MonoBehaviour
     {
-        [SerializeField] private float minTimeBeforeSpawn;
-        [SerializeField] private float maxTimeBeforeSpawn;
-
         [SerializeField] private float minYPosSpawn = -1f;
         [SerializeField] private float maxYPosSpawn = 1f;
         
         [SerializeField] private GameObject missilePrefab;
         [SerializeField] private Transform missileSpawnPoint;
 
+        
+        [SerializeField] private float difficultyChangeRate = 25f;
+
+        [SerializeField] private List<float> timesBeforeSpawn = new();
+        private float _timeBeforeSpawn;
+        
+        private float _nextDifficultyChangeTime;
+
         private void Start()
         {
+            _timeBeforeSpawn = timesBeforeSpawn[0];
             StartCoroutine(SpawnMissiles());
+        }
+        
+        private void Update()
+        {
+            if (timesBeforeSpawn.Count > 0)
+            {
+                if (Time.time > _nextDifficultyChangeTime)
+                {   
+                    _nextDifficultyChangeTime = Time.time + difficultyChangeRate;
+                    IncreaseTimeDifficulty();
+                }
+            }
+        }
+        
+        private void IncreaseTimeDifficulty()
+        {
+            _timeBeforeSpawn = timesBeforeSpawn[0];
+            timesBeforeSpawn.RemoveAt(0);
         }
 
         private IEnumerator SpawnMissiles()
         {
             while (true)
             {
+                yield return new WaitForSeconds(_timeBeforeSpawn);
+
                 float spawnHeight = Random.Range(minYPosSpawn, maxYPosSpawn);
                 Vector2 spawnPosition = new Vector2(missileSpawnPoint.position.x, spawnHeight);
                 
                 // Instantiate missile with a random height
                 var spawnedMissile = Instantiate(missilePrefab, spawnPosition, Quaternion.identity);
                 spawnedMissile.transform.SetParent(missileSpawnPoint);
-
-                float timeBeforeSpawn = Random.Range(minTimeBeforeSpawn, maxTimeBeforeSpawn);
-                yield return new WaitForSeconds(timeBeforeSpawn);
             }
         }
     }
