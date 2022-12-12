c2527732e08dab8a4edd925b3007f9c417416cd6
JackSinnott <c00242525@itcarlow.ie>
2022-12-12 11:27:22 +0000

    Package installed and enemy swapped out for black void capsule
    ChasingEnemy is working and beautiful
    
    Changed collider slightly

--- Assets/Scripts/ChasingEnemy.cs
+++ Assets/Scripts/ChasingEnemy.cs
@@ -1,4 +1,4 @@
-using System;
+using Spine.Unity;
 using System.Collections;
 using UnityEngine;
 
@@ -22,15 +22,23 @@ public class ChasingEnemy : MonoBehaviour
 
     [SerializeField] private int _duration = 5;
 
+    [SerializeField] private SkeletonAnimation _skeletonAnimation;
+
+    [SerializeField] bool _speedingUp = false;
+    [SerializeField] bool _slowingDown = false;
+
 
     private void Start()
     {
         float playerXPosition = _player.transform.position.x;
         _stablePosition = new Vector2(playerXPosition - OffsetFromPlayerStable, transform.position.y);
         transform.position = _stablePosition;
+
+        _skeletonAnimation = GetComponentInChildren<SkeletonAnimation>();
         
         _unstablePosition = new Vector2(playerXPosition - OffsetFromPlayerUnstable, transform.position.y);
 
+        _skeletonAnimation.timeScale = 2.0f;
         _pds = GameObject.FindWithTag("Player").GetComponent<PlayerDamageSystem>();
     }
 
@@ -42,24 +50,36 @@ public class ChasingEnemy : MonoBehaviour
             if(!m_angered)
                 StartCoroutine(LerpPosition(_unstablePosition, _duration));
         }
+
+        if (_speedingUp && !_slowingDown)
+            _skeletonAnimation.timeScale = 8.0f;
+        else if (_slowingDown && !_speedingUp)
+            _skeletonAnimation.timeScale = 1.0f;
+        else
+            _skeletonAnimation.timeScale = 2.0f;
     }
 
     IEnumerator LerpPosition(Vector2 targetPosition, float duration)
     {
         float time = 0;
+        _speedingUp = true;
         while (time < duration)
         {
             transform.position = Vector2.Lerp(_stablePosition, targetPosition, time / duration);
+            
             time += Time.deltaTime;
             yield return null;
         }
         time = 0;
+        _speedingUp = false;
+        _slowingDown = true;
         while (time < duration)
         {
             transform.position = Vector2.Lerp(targetPosition, _stablePosition, time / duration);
             time += Time.deltaTime;
             yield return null;
         }
+        _slowingDown = false;
     }
 
     public int GetDuration()
