95d4f26218bc144c7cec57e62d1b9ef7d148292f
JackSinnott <c00242525@itcarlow.ie>
2022-12-12 13:13:46 +0000

    Merge issue im Dev/Animations so created this as a quick workaround jira_id: RWMPA22-110

--- Assets/Scripts/ChasingEnemy.cs
+++ Assets/Scripts/ChasingEnemy.cs
@@ -1,4 +1,4 @@
-using System;
+using Spine.Unity;
 using System.Collections;
 using UnityEngine;
 
@@ -6,7 +6,7 @@ public class ChasingEnemy : MonoBehaviour
 {
     [field: SerializeField]
     public float OffsetFromPlayerStable { get; private set; } = 6f;
-    
+
     [field: SerializeField]
     public float OffsetFromPlayerUnstable { get; private set; } = 2f;
 
@@ -22,15 +22,23 @@ public class ChasingEnemy : MonoBehaviour
 
     [SerializeField] private int _duration = 5;
 
+    [SerializeField] private SkeletonAnimation _skeletonAnimation;
+
+    [SerializeField] bool _speedingUp = false;
+    [SerializeField] bool _slowingDown = false;
+
 
     private void Start()
     {
         float playerXPosition = _player.transform.position.x;
         _stablePosition = new Vector2(playerXPosition - OffsetFromPlayerStable, transform.position.y);
         transform.position = _stablePosition;
-        
+
+        _skeletonAnimation = GetComponentInChildren<SkeletonAnimation>();
+
         _unstablePosition = new Vector2(playerXPosition - OffsetFromPlayerUnstable, transform.position.y);
 
+        _skeletonAnimation.timeScale = 2.0f;
         _pds = GameObject.FindWithTag("Player").GetComponent<PlayerDamageSystem>();
     }
 
@@ -39,27 +47,39 @@ public class ChasingEnemy : MonoBehaviour
         if (_pds._playerDamage == 1)
         {
             _pds.SetDamage(0);
-            if(!m_angered)
+            if (!m_angered)
                 StartCoroutine(LerpPosition(_unstablePosition, _duration));
         }
+
+        if (_speedingUp && !_slowingDown)
+            _skeletonAnimation.timeScale = 8.0f;
+        else if (_slowingDown && !_speedingUp)
+            _skeletonAnimation.timeScale = 1.0f;
+        else
+            _skeletonAnimation.timeScale = 2.0f;
     }
 
     IEnumerator LerpPosition(Vector2 targetPosition, float duration)
     {
         float time = 0;
+        _speedingUp = true;
         while (time < duration)
         {
             transform.position = Vector2.Lerp(_stablePosition, targetPosition, time / duration);
+
             time += Time.deltaTime;
             yield return null;
         }
         time = 0;
+        _speedingUp = false;
+        _slowingDown = true;
         while (time < duration)
         {
             transform.position = Vector2.Lerp(targetPosition, _stablePosition, time / duration);
             time += Time.deltaTime;
             yield return null;
         }
+        _slowingDown = false;
     }
 
     public int GetDuration()
