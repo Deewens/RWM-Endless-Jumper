c3211b1e2ac1b7cbbb02c6161a2a5573552fbcc7
Adrien Dudon <dudonadrien@gmail.com>
2022-12-12 12:09:23 +0000

    A warning sign appear just before the next missile spawn (same height as the next missile) RWMPA22-98

--- Assets/Scripts/Missiles/MissilesManager.cs
+++ Assets/Scripts/Missiles/MissilesManager.cs
@@ -13,17 +13,21 @@ namespace Missiles
         
         [SerializeField] private GameObject missilePrefab;
         [SerializeField] private Transform missileSpawnPoint;
-
+        
+        [SerializeField] private GameObject warningSignPrefab;
         
         [SerializeField] private float difficultyChangeRate = 25f;
-
         [SerializeField] private List<float> timesBeforeSpawn = new();
-        private float _timeBeforeSpawn;
         
+        private float _timeBeforeSpawn;
         private float _nextDifficultyChangeTime;
-
+        
+        private Camera _mainCamera;
+        
         private void Start()
         {
+            _mainCamera = Camera.main;
+
             _timeBeforeSpawn = timesBeforeSpawn[0];
             StartCoroutine(SpawnMissiles());
         }
@@ -50,11 +54,22 @@ namespace Missiles
         {
             while (true)
             {
-                yield return new WaitForSeconds(_timeBeforeSpawn);
-
-                float spawnHeight = Random.Range(minYPosSpawn, maxYPosSpawn);
-                Vector2 spawnPosition = new Vector2(missileSpawnPoint.position.x, spawnHeight);
+                yield return new WaitForSeconds(_timeBeforeSpawn / 2);
                 
+                // Generate random height for missile to spawn
+                var spawnHeight = Random.Range(minYPosSpawn, maxYPosSpawn);
+                var spawnPosition = new Vector2(missileSpawnPoint.position.x, spawnHeight);
+                
+                // Spawn warning sign before missile
+                var cameraHorizontalHalfSize = _mainCamera.orthographicSize * Screen.width / Screen.height;
+                var warningSizeSpawnPos = new Vector2(cameraHorizontalHalfSize - 0.5f, spawnHeight);
+                var warningSignGO = Instantiate(warningSignPrefab, warningSizeSpawnPos, Quaternion.identity);
+                
+                yield return new WaitForSeconds(_timeBeforeSpawn / 2);
+                
+                // Destroy warning sign just before instantiating missile
+                Destroy(warningSignGO);
+
                 // Instantiate missile with a random height
                 var spawnedMissile = Instantiate(missilePrefab, spawnPosition, Quaternion.identity);
                 spawnedMissile.transform.SetParent(missileSpawnPoint);
